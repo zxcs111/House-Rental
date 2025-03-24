5ae5ad40935e067dbd6d0b5e0f151d003bfd7663
zxcs111 <arjheezyb@gmail.com>
2025-03-24 22:27:28 +0800

    Added space between the edit and delete icon

--- resources/views/landlord/propertylisting.blade.php
+++ resources/views/landlord/propertylisting.blade.php
@@ -109,7 +109,7 @@
           <div class="container">
             <div class="row no-gutters slider-text js-fullheight align-items-end justify-content-start">
               <div class="col-md-9 ftco-animate pb-5">
-                <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>Property Listing <i class="ion-ios-arrow-forward"></i></span></p>
+                <p class="breadcrumbs"><span class="mr-2"><a href="{{ route ('home') }}">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>Property Listing <i class="ion-ios-arrow-forward"></i></span></p>
                 <h1 class="mb-3 bread">Your Property Listings</h1>
               </div>
             </div>
@@ -199,7 +199,7 @@
                             </td>
                             <td>
                               <div class="btn-group" role="group">
-                                  <button class="btn btn-sm btn-primary edit-property" 
+                                  <button class="btn btn-sm btn-primary edit-property mr-2" 
                                           data-toggle="modal" 
                                           data-target="#createPropertyModal"
                                           data-edit="true"
